4c9cd9a93dbf514aa93dbe1312e11ecb257dc09e
Peter Barker <pb-temper@barker.dropbear.id.au>
2012-06-10 16:53:48 +1000

    return busnum/devnum from bus_and_dev_nums_from_for_device_and_tree rather than print them

--- FindTempers.pm
+++ FindTempers.pm
@@ -7,6 +7,14 @@ use DirHandle qw ();
 use FileHandle qw ();
 use POSIX qw ( O_RDONLY );
 
+package Temper;
+
+# we'll use this later...
+1;
+
+
+package FindTempers;
+
 sub slurp {
   my ($filename) = @_;
 
@@ -18,7 +26,7 @@ sub slurp {
   $content;
 }
 sub find_devices_by_idVendor_and_idProduct {
-  my ($basepath,$idVendor,$idProduct) = @_;
+  my ($class,$basepath,$idVendor,$idProduct) = @_;
   my $dh = DirHandle->new($basepath) or
     die "Failed to open ($basepath): $!";
 
@@ -48,10 +56,11 @@ sub find_devices_by_idVendor_and_idProduct {
   return @entries;
 }
 
+sub bus_id_and_device_id_for_device_name {
+  my ($class,$device_name) = @_;
 
-package Temper;
-
-package FindTempers;
+  
+}
 
 my $temperA = bless {
   idVendor => '0c45',
@@ -76,7 +85,9 @@ my $basepath ="/sys/bus/usb/devices";
 my $hub_idVendor = '05e3';
 my $hub_idProduct = '0608';
 sub find_tempers {
-  my @devices = &find_devices_by_idVendor_and_idProduct($basepath,$hub_idVendor,$hub_idProduct);
+  my ($class) = @_;
+
+  my @devices = $class->find_devices_by_idVendor_and_idProduct($basepath,$hub_idVendor,$hub_idProduct);
 #  print STDERR "Got (@devices)\n";
   die "Found more than one device matching Vendor/Product $hub_idVendor:$hub_idProduct)" if @devices > 1;
   if (not @devices) {
@@ -88,7 +99,11 @@ sub find_tempers {
     die "unable to match device ($device) against device tree";
   }
 
-  &bus_and_dev_nums_from_for_device_and_tree($basepath,$device,$device_tree)
+  my @pairs = &bus_and_dev_nums_from_for_device_and_tree($basepath,$device,$device_tree);
+  foreach my $pair (@pairs) {
+    my ($busnum,$devicenum) = @$pair;
+    print "$busnum $devicenum\n";
+  }
 }
 
 sub bus_and_dev_nums_from_for_device_and_tree {
@@ -96,8 +111,10 @@ sub bus_and_dev_nums_from_for_device_and_tree {
 
   if (! -e "$basepath/$device") {
     print STDERR "Whoops - no $basepath/$device\n";
-    return 0;
+    return ();
   }
+
+  my @pairs = ();
   foreach my $key (keys %$tree) {
     my $path = "$device.$key";
     if (ref($tree->{$key}) eq 'Temper') {
@@ -112,13 +129,14 @@ sub bus_and_dev_nums_from_for_device_and_tree {
       chomp $devnum;
       my $busnum = &slurp("$basepath/$path/busnum");
       chomp $busnum;
-      print "$busnum $devnum\n";
+#      print "$busnum $devnum\n";
+      push @pairs,[$busnum,$devnum];
       next;
     }
-    &bus_and_dev_nums_from_for_device_and_tree($basepath,$path,$tree->{$key});
+    push @pairs, &bus_and_dev_nums_from_for_device_and_tree($basepath,$path,$tree->{$key});
   }
 
-  return 1;
+  return @pairs;
 }
 # sub walk_tree {
 #   my ($basepath,$device,$tree) = @_;
